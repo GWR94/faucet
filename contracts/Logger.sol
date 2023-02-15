ca1642daa291cbbbedbf688cb730f8b61cbc30fa
James Gower <jgower.dev@gmail.com>
2023-02-15 15:30:49 +0000

    created part of front-end for faucet, refactored faucet contract

--- contracts/Logger.sol
+++ contracts/Logger.sol
@@ -5,6 +5,13 @@ pragma solidity >=0.4.22 <0.9.0;
   ABSTRACT
   Is a way for designer to say that any 'child' of the abstract contract has to
   implement specified methods to use them.
+
+  INHERITANCE
+  private   function can only be used from within the current contract
+  internal  function can be called from within the contract, or the contracts that are inheriting
+            the current contract.
+  public    function can be called from anywhere, inside or outside the smart contract.
+  external  function can only be called from outside of the smart contract, not inside it.
  */
 
 abstract contract Logger {
--- src/App.js
+++ src/App.js
@@ -1,5 +1,78 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Web3 from "web3";
+import detectProvider from "@metamask/detect-provider";
+
 function App() {
-  return <div className="App">Hello World!</div>;
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [web3API, setWeb3API] = useState({
+    provider: null,
+    web3: null,
+  });
+
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectProvider();
+      if (provider) {
+        setWeb3API({
+          web3: new Web3(provider),
+          provider,
+        });
+      } else {
+        console.error("Please install Metamask.");
+      }
+    };
+    loadProvider();
+  }, []);
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3API.web3.eth.getAccounts();
+      const account = accounts[0];
+      if (account) {
+        setCurrentAccount(account);
+        const balance = await web3API.web3.eth.getBalance(account);
+        const balanceInETH = web3API.web3.utils.fromWei(balance, "ether");
+        setBalance(balanceInETH);
+      }
+    };
+    web3API.web3 && getAccount();
+  }, [web3API.web3]);
+
+  return (
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <span>
+            <strong>Account: </strong>
+          </span>
+          {currentAccount ? (
+            <>
+              <h1>{currentAccount}</h1>
+              <div className="balance-view is-size-2">
+                Current Balance: <strong>{balance}</strong> ETH
+              </div>
+              <div className="button-container">
+                <button className="button is-link mr-2">Donate</button>
+                <button className="button is-primary">Withdraw</button>
+              </div>
+            </>
+          ) : (
+            <button
+              className="is-link"
+              onClick={() =>
+                web3API.provider.request({ method: "eth_requestAccounts" })
+              }
+            >
+              Connect
+            </button>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
